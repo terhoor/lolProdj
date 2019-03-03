d84d35db37f396464c3c0eedf9ef981e0e004c09
elivanov.denis <elivanov.denis@mediasoft.team>
2019-03-03 23:04:11 +0400

    add new

--- src/js/dynamicScript.js
+++ src/js/dynamicScript.js
@@ -1,8 +1,6 @@
 import $ from "jquery";
-
+import { isArray } from "util";
 var dataInfoOrg;
-
-
 
 (function () {
   var urlParams = new URLSearchParams(window.location.search);
@@ -16,6 +14,7 @@ var dataInfoOrg;
       async: false,
       success: function (result) {
         res = JSON.parse(result);
+        console.log(res);
       }
     });
     return res;
@@ -30,7 +29,6 @@ var dataInfoOrg;
       async: false,
       success: function (result) {
         typeName = JSON.parse(result);
-        console.log(typeName);
       }
     });
     var $nav = $('.js-nav');
@@ -45,7 +43,8 @@ var dataInfoOrg;
     </li>`));
 
     for (var key in typeName) {
-      if (dataInfoOrg[key].length) {
+      console.log(dataInfoOrg[key].length);
+      if (dataInfoOrg[key].length ||!isArray(dataInfoOrg[key])) {
         var $newItem = $navItem.clone();
         var word = typeName[key].charAt(0).toUpperCase() + typeName[key].slice(1);
         $newItem.find('.js-nav-link').attr('data-place', key).text(word).attr('href', `#${key}`);
@@ -61,6 +60,8 @@ var dataInfoOrg;
 
 export function funcDynamic() {
   var hash = $(this).data('place') || window.location.hash.slice(1) || 'main';
+  var $dynamicDiv = $('.dynamic-place');
+
   $('.js-nav .active').removeClass('active');
   if (this === undefined) {
     $('.js-nav .nav-link').filter(function () {
@@ -71,54 +72,220 @@ export function funcDynamic() {
     $(this).addClass('active');
   }
 
-  var $dynamicDiv = $('.dynamic-place');
-  if (hash === 'books') {
-    $dynamicDiv.load('books.html .books', function () {
-      var $booksBlock = $('.block-books');
-      var bookStruc = $('.block-book').detach();
+  $('.background-img').css({
+    'background': `url(./uploads/${hash}.jpg)`,
+    'background-size': 'cover'
+  });
+  switch (hash) {
+    case 'main':
+      $dynamicDiv.load('main.html .main', function () {
+        $('.header .header-org').text(dataInfoOrg.organization.nameOrganization);
+        $('.header .header-doc').text(dataInfoOrg.organization.numberDocumentation);
+        $('.header .header-status').text(dataInfoOrg.organization.statusOrganization);
+        $('.main-people .block-director')
+          .find('.surname')
+          .text(dataInfoOrg.director.surname).end()
+          .find('.name')
+          .text(dataInfoOrg.director.name).end()
+          .find('.patronymic')
+          .text(dataInfoOrg.director.surname);
 
-      dataInfoOrg.books.forEach(function (book) {
-        var $newBook = $(bookStruc).clone();
-
-        $newBook
-          .find('.name').text(book.name).end()
-          .find('.author').text(book.author).end()
-          .find('.publish').text(book.publish).end()
-          .find('.year').text(book.year).end();
-
-        $booksBlock.append($newBook);
-        $booksBlock.append("</br>");
+        $('.main-people .block-responsible')
+          .find('.surname')
+          .text(dataInfoOrg.responsible.surname).end()
+          .find('.name')
+          .text(dataInfoOrg.responsible.name).end()
+          .find('.patronymic')
+          .text(dataInfoOrg.responsible.surname).end()
+          .find('.telephone')
+          .text(dataInfoOrg.responsible.telephone);
       });
-    });
 
+      break;
 
-  } else if (hash === 'main') {
-    $dynamicDiv.load('main.html .main', function () {
-      $('.header .header-org').text(dataInfoOrg.organization.nameOrganization);
-      $('.header .header-doc').text(dataInfoOrg.organization.numberDocumentation);
-      $('.main-people .block-director')
-        .find('.surname')
-        .text(dataInfoOrg.director.surname).end()
-        .find('.name')
-        .text(dataInfoOrg.director.name).end()
-        .find('.patronymic')
-        .text(dataInfoOrg.director.surname);
+    case 'books':
+      $dynamicDiv.load('books.html .books', function () {
+        var $booksBlock = $('.block-books');
+        var bookStruc = $('.block-book').detach();
 
-      $('.main-people .block-responsible')
-        .find('.surname')
-        .text(dataInfoOrg.responsible.surname).end()
-        .find('.name')
-        .text(dataInfoOrg.responsible.name).end()
-        .find('.patronymic')
-        .text(dataInfoOrg.responsible.surname).end()
-        .find('.telephone')
-        .text(dataInfoOrg.responsible.telephone);
+        dataInfoOrg.books.forEach(function (book) {
+          var $newBook = $(bookStruc).clone();
 
-    });
+          $newBook
+            .find('.name').text(book.name).end()
+            .find('.author').text(book.author).end()
+            .find('.publish').text(book.publish).end()
+            .find('.year').text(book.year).end();
 
+          $booksBlock.append($newBook);
+          $booksBlock.append("</br>");
+        });
+      });
+      break;
+    case 'teachers':
+      $dynamicDiv.load('teachers.html .teachers', function () {
+        var $teachersBlock = $('.block-teachers');
+        var $teacherStruc = $('.block-teacher').detach();
 
+        dataInfoOrg.teachers.forEach(function (teacher) {
+          var $newTeacher = $($teacherStruc).clone();
+
+          $newTeacher
+            .find('.name').text(teacher.name).end()
+            .find('.surname').text(teacher.surname).end()
+            .find('.patronymic').text(teacher.patronymic).end();
+
+          $teachersBlock.append($newTeacher);
+          $teachersBlock.append("</br>");
+        });
+
+      });
+      break;
+
+    case 'additionalInfo':
+      $dynamicDiv.load('additionalInfo.html .additionalInfo', function () {
+        var $additionalInfoBlock = $('.block-additionalInfo');
+        var $description = $('.description').detach();
+
+        // var $newInfo = $($description).clone();
+
+        $description.text(dataInfoOrg.additionalInfo.description);
+
+        $additionalInfoBlock.append($description);
+        $additionalInfoBlock.append("</br>");
+
+      });
+      break;
+
+    case 'museums':
+      $dynamicDiv.load('museums.html .museums', function () {
+        var $museumsBlock = $('.block-museums');
+        var $museumStruc = $('.block-museum').detach();
+
+        dataInfoOrg.museums.forEach(function (museum) {
+          var $newMuseum = $($museumStruc).clone();
+
+          $newMuseum
+            .find('.description').text(museum.description).end()
+            .find('.exposition').text(museum.exposition).end()
+            .find('.head').text(museum.head).end();
+
+          $museumsBlock.append($newMuseum);
+        });
+
+      });
+      break;
+
+    case 'cabinets':
+      $dynamicDiv.load('cabinets.html .cabinets', function () {
+        var $cabinetsBlock = $('.block-cabinets');
+        var $cabinetStruc = $('.block-cabinet').detach();
+
+        dataInfoOrg.cabinets.forEach(function (cabinet) {
+          var $newCabinet = $($cabinetStruc).clone();
+
+          $newCabinet
+            .find('.description').text(cabinet.description).end()
+            .find('.head').text(cabinet.exposition).end()
+
+          $cabinetsBlock.append($newCabinet);
+        });
+
+      });
+      break;
+
+    case 'others':
+      $dynamicDiv.load('others.html .others', function () {
+        var $othersBlock = $('.block-others');
+        var $description = $('.description').detach();
+
+        // var $newInfo = $($description).clone();
+        $description.text(dataInfoOrg.others.description);
+
+        $othersBlock.append($description);
+        $othersBlock.append("</br>");
+
+      });
+      break;
+
+    case 'subjects':
+      $dynamicDiv.load('subjects.html .subjects', function () {
+        var $subjectsBlock = $('.block-subjects');
+        var subjectStruc = $('.block-subject').detach();
+
+        dataInfoOrg.subjects.forEach(function (subject) {
+          var $newSubject = $(subjectStruc).clone();
+
+          $newSubject
+            .find('.title').text(subject.title).end()
+            .find('.level').text(subject.level).end();
+
+          $subjectsBlock.append($newSubject);
+          $subjectsBlock.append("</br>");
+        });
+      });
+      break;
+
+      case 'societies':
+      $dynamicDiv.load('societies.html .societies', function () {
+        var $societiesBlock = $('.block-societies');
+        var societyStruc = $('.block-society').detach();
+
+        dataInfoOrg.societies.forEach(function (society) {
+          var $newSociety = $(societyStruc).clone();
+
+          $newSociety
+            .find('.name').text(society.name).end()
+            .find('.class').text(society.class).end()
+            .find('.head').text(society.head).end()
+            .find('.description').text(society.description).end();
+
+          $societiesBlock.append($newSociety);
+          $societiesBlock.append("</br>");
+        });
+      });
+      break;
+      case 'collectives':
+      $dynamicDiv.load('collectives.html .collectives', function () {
+        var $collectivesBlock = $('.block-collectives');
+        var collectiveStruc = $('.block-collective').detach();
+
+        dataInfoOrg.collectives.forEach(function (collective) {
+          var $newCollective = $(collectiveStruc).clone();
+
+          $newCollective
+            .find('.name').text(collective.name).end()
+            .find('.ageChildren').text(collective.ageChildren).end()
+            .find('.head').text(collective.head).end()
+            .find('.description').text(collective.description).end();
+
+          $collectivesBlock.append($newCollective);
+          $collectivesBlock.append("</br>");
+        });
+      });
+      break;
+
+      case 'events':
+      $dynamicDiv.load('events.html .events', function () {
+        var $eventsBlock = $('.block-events');
+        var eventStruc = $('.block-event').detach();
+
+        dataInfoOrg.events.forEach(function (event) {
+          var $newEvent = $(eventStruc).clone();
+
+          $newEvent
+            .find('.name').text(event.name).end()
+            .find('.level').text(event.level).end()
+            .find('.form').text(event.form).end()
+            .find('.date').text(event.date).end();
+
+          $eventsBlock.append($newEvent);
+          $eventsBlock.append("</br>");
+        });
+      });
+      break;
   }
 
-  $dynamicDiv.empty();
+
 
 }
